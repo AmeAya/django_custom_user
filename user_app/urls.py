2b5496a289e1707d66fce930774e102842cdb454
AmeAya <deobackstep@gmail.com>
2024-03-15 20:17:40 +0600

    reg

--- user_app/urls.py
+++ user_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', homeView, name='home_url'),
     path('logout', logOutView, name='log_out_url'),
     path('login', logInView, name='log_in_url'),
+    path('register', registerView, name='register_url'),
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -27,3 +27,28 @@ def logInView(request):
             return redirect('home_url')
         else:
             return redirect('log_in_url')
+
+
+def registerView(request):
+    if request.method == 'GET':
+        return render(request, 'registration_template.html')
+    elif request.method == 'POST':
+        from .models import CustomUser
+        input_email = request.POST.get('email')
+        input_pass1 = request.POST.get('pass1')
+        input_pass2 = request.POST.get('pass2')
+        this_user = CustomUser.objects.filter(email=input_email)
+        if len(this_user) > 0:  # Если найден юзер с таким email. Не дать зарегистрироваться так как email занят
+            context = {
+                'message': 'This email already taken!'
+            }
+            return render(request, 'registration_template.html', context=context)
+        if input_pass1 != input_pass2:  # Если пароли не сходятся
+            context = {
+                'message': 'Passwords doesn`t match!',
+                'email': input_email
+            }
+            return render(request, 'registration_template.html', context=context)
+        user = CustomUser.objects.create_user(email=input_email, password=input_pass1)
+        login(request, user)
+        return redirect('home_url')
